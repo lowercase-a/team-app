529ca7db3a0bc1e9a64f33e53a44d6050fd292fb
Uwana <uwikaiddi@gmail.com>
2019-04-16 19:19:03 -0500

    Add chocolate controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # verb "/urls" => "namespace/controllers#action"
   namespace :api do
     get '/teams' => 'teams#index'
+
+    get '/chocolates' => 'chocolates#index'
   end
 end
   
