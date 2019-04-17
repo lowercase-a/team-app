cabdcf25b1b57968de5a184e7ce37edfe5caaee8
joeyharbert <joey.harbert@gmail.com>
2019-04-16 19:14:03 -0500

    Adds unicorn controller and index action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # verb "/urls" => "namespace/controllers#action"
   namespace :api do
     get '/teams' => 'teams#index'
+    get '/unicorns' => 'unicorns#index'
   end
 end
   
